c7253cc37f5211860661bc1e11ed11c033adbe91
Abdullah Shahid <abdullahshahid984@gmail.com>
2025-07-17 00:34:23 +0500

    commit

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ✅ important for GitHub Pages or custom domain
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
